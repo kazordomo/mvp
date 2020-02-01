a8917cf9e7ba37d707c59f7c758e9a02d5347ee9
Zak <zak.eklund@live.se>
2020-02-01 12:25:07 +0100

    chore: firebase dev setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import * as firebase from 'firebase';
-import keys from './config/keys';
-import uuid from 'uuid';
 import styled from 'styled-components';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import keys from './config/keys';
 import { getById, updateById, setById } from './firebase/fetch';
 import { isEmptyObj } from './utils';
-import { 
-	populatePlayers, 
-	populateUsers, 
-	populateRatingOccasions 
-} from './actions';
+import { populatePlayers, populateUsers, populateRatingOccasions } from './actions';
 
 import Login from './components/login/Login';
 import Admin from './components/admin/Admin';
@@ -24,7 +19,6 @@ import Loading from './components/_shared/Loading';
 // TODO: Handle errors.
 
 class App extends Component {
-
 	constructor(props) {
 		super(props);
 
@@ -35,26 +29,24 @@ class App extends Component {
 			players: [],
 			ratingOccasions: [],
 			isGuest: false,
-		}
+		};
 	}
 
 	async componentDidMount() {
 		// Regsiter fb-app. If already registered - skip.
-		if (!firebase.apps.length)
-			firebase.initializeApp(keys.firebaseConfig);
+		if (!firebase.apps.length) firebase.initializeApp(keys.firebaseConfig);
 
 		// This func gets called when the app is fired up or when a user signs in/out.
 		firebase.auth().onAuthStateChanged(async user => {
 			if (user) {
 				const idTokenResult = await user.getIdTokenResult();
 				const snapshot = await getById('users', user.uid);
-				const userData ={
+				const userData = {
 					...snapshot.data(),
 					admin: idTokenResult.claims.admin ? idTokenResult.claims.admin : false,
 				};
 				this.setState({ user: userData, isFetching: true }, this.populateData);
-			}
-			else {
+			} else {
 				this.setState({ isFetching: false, user: {} }, this.populateData);
 			}
 		});
@@ -71,88 +63,105 @@ class App extends Component {
 	}
 
 	getActiveRatingOccasion = () => this.state.ratingOccasions.find(occasion => occasion.active);
+
 	// If the player/person got an account, we will use the user.id when we enter the profile. Otherwise we will use the player.id/nr.
 	getProfileId = nr => {
 		const user = this.state.users.find(user => parseInt(user.playerNumber) === nr);
 		return user ? user.id : nr;
-	}
-	
+	};
+
 	onOpenRating = async opponents => {
 		try {
-			const id = uuid();
+			const id = 1;
 			// Get the last rounds "round-number" and add 1. If first round - use 1 instead of 0.
-			const round = this.state.ratingOccasions.length ? 
-				(this.state.ratingOccasions.sort((a, b) => b.round - a.round)[0].round + 1) : 1;
+			const round = this.state.ratingOccasions.length
+				? this.state.ratingOccasions.sort((a, b) => b.round - a.round)[0].round + 1
+				: 1;
 
 			const newRatingOccasion = {
 				id,
 				opponents,
 				round,
 				active: true,
-			}
+			};
 			await setById('ratingOccasions', id, newRatingOccasion);
-			this.setState({ ratingOccasions: [ ...this.state.ratingOccasions, newRatingOccasion ] });
-		} catch(err) {
+			this.setState({ ratingOccasions: [...this.state.ratingOccasions, newRatingOccasion] });
+		} catch (err) {
 			console.log(err);
 		}
-	}
+	};
 
 	onCloseRating = async () => {
 		try {
 			// TODO: map
-			let ratingOccasions = [ ...this.state.ratingOccasions ];
-			let ratingOccasion = this.getActiveRatingOccasion();
+			const ratingOccasions = [...this.state.ratingOccasions];
+			const ratingOccasion = this.getActiveRatingOccasion();
 			ratingOccasion.active = false;
 			ratingOccasions[ratingOccasions.indexOf(ratingOccasion)] = ratingOccasion;
 			await updateById('ratingOccasions', ratingOccasion.id, ratingOccasion);
 			this.setState({ ratingOccasions });
-		} catch(err) {
+		} catch (err) {
 			console.log(err);
 		}
-	}
+	};
 
 	enterAsGuest = () => this.setState({ isGuest: true });
 
 	render() {
-		if (this.state.isFetching)
-            return <Loading />
-		
-		if (isEmptyObj(this.state.user) && !this.state.isGuest)
-			return <Login onEnterAsGuest={this.enterAsGuest} />
+		if (this.state.isFetching) return <Loading />;
+
+		if (isEmptyObj(this.state.user) && !this.state.isGuest) return <Login onEnterAsGuest={this.enterAsGuest} />;
 
 		return (
 			<Router>
 				<AppContainer>
-					<Route exact path='/' render={() => 
-						<Home user={this.state.user} ratingOccasion={this.getActiveRatingOccasion()} />
-					}/>
-					<Route path='/profile/:id' render={props => 
-						<Profile {...props} users={this.state.users} players={this.state.players} />
-					}/>
-					<Route path='/rate' render={() => 
-						<Rate user={this.state.user} players={this.state.players} ratingOccasion={this.getActiveRatingOccasion()}/>
-					}/>
-					<Route path='/leaderboard' render={() => 
-						<Leaderboard players={this.state.players} getProfileId={this.getProfileId} />
-					}/>
-					<Route path='/statistics' render={() => 
-						<Statistics 
-							players={this.state.players} 
-							users={this.state.users} 
-							ratingOccasions={this.state.ratingOccasions} 
-						/>
-					}/>
-					<Route path='/admin' render={() => 
-						<Admin 
-							user={this.state.user}
-							ratingOccasion={this.getActiveRatingOccasion()}
-							onOpenRating={this.onOpenRating}
-							onCloseRating={this.onCloseRating}
-						/>
-					}/>
+					<Route
+						exact
+						path="/"
+						render={() => <Home user={this.state.user} ratingOccasion={this.getActiveRatingOccasion()} />}
+					/>
+					<Route
+						path="/profile/:id"
+						render={props => <Profile {...props} users={this.state.users} players={this.state.players} />}
+					/>
+					<Route
+						path="/rate"
+						render={() => (
+							<Rate
+								user={this.state.user}
+								players={this.state.players}
+								ratingOccasion={this.getActiveRatingOccasion()}
+							/>
+						)}
+					/>
+					<Route
+						path="/leaderboard"
+						render={() => <Leaderboard players={this.state.players} getProfileId={this.getProfileId} />}
+					/>
+					<Route
+						path="/statistics"
+						render={() => (
+							<Statistics
+								players={this.state.players}
+								users={this.state.users}
+								ratingOccasions={this.state.ratingOccasions}
+							/>
+						)}
+					/>
+					<Route
+						path="/admin"
+						render={() => (
+							<Admin
+								user={this.state.user}
+								ratingOccasion={this.getActiveRatingOccasion()}
+								onOpenRating={this.onOpenRating}
+								onCloseRating={this.onCloseRating}
+							/>
+						)}
+					/>
 				</AppContainer>
 			</Router>
-		)
+		);
 	}
 }
 
