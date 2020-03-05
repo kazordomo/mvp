ddeddca825de23dc2fdf535eb59ae1c100d71b90
Zak <zak.eklund@live.se>
2020-03-05 20:02:44 +0100

    feat: pointsinfo with new animation hook

--- src/components/_shared/PointsInfo.js
+++ src/components/_shared/PointsInfo.js
@@ -1,49 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
-import colors from '../../assets/colors';
-import Animation from '../hoc/Animation';
 
-const PointsInfo = ({ activeAnimation }) => (
-    <Info active={activeAnimation}>
-        <div>1p: <span></span></div>
-        <div>2p: <span></span></div>
-        <div>3p: <span></span></div>
-    </Info>
-);
+import colors from '../../assets/colors';
+
+import useAnimation from '../hooks/animation';
 
 const Info = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     margin-bottom: 30px;
-    margin-left: ${props=>props.active?'0':'-200px'};
-    transform: scale(${props=>props.active?'1':'1.4'});
+    margin-left: ${props => props.active ? '0' : '-200px'};
+    transform: scale(${props => props.active ? '1' : '1.4'});
     transition: margin 650ms cubic-bezier(.36,.36,.26,.86), transform 650ms ease-out;
-
-    div {
-        align-items: center;
-        display: flex;
-        color: #fff;
-
-        span {
-            margin-left: 10px;
-        }
-        :nth-child(1) span {
-            background-color: ${colors.pointvalueone()};
-        }
-        :nth-child(2) span {
-            background-color: ${colors.pointvaluetwo()};
-        }
-        :nth-child(3) span {
-            background-color: ${colors.pointvaluethree()};
-        }
-    }
-
-    div span {
-        display: block;
-        height: 15px;
-        width: 50px;
-    }
 `;
 
-export default Animation(PointsInfo, 10);
+const Item = styled.div`
+	align-items: center;
+	display: flex;
+	color: #fff;
+`;
+
+const ColorBar = styled.div`
+	margin-left: 10px;
+	display: block;
+	height: 15px;
+	width: 50px;
+	background-color: ${props => props.color};
+`;
+
+
+const PointsInfo = () => (
+	<Info active={!useAnimation()}>
+		<Item>1p: <ColorBar color={colors.pointvalueone()} /></Item>
+		<Item>2p: <ColorBar color={colors.pointvaluetwo()} /></Item>
+		<Item>3p: <ColorBar color={colors.pointvaluethree()} /></Item>
+	</Info>
+);
+
+export default PointsInfo;
